4c4c88ebb347ad4f3c1974747fd010156a408f9a
Alfa <annekatrin.dunkel@gmx.de>
2020-03-02 15:15:31 +0100

    Set VC depending on if loaded for first time or not

--- Drink Enough/SceneDelegate.cs
+++ Drink Enough/SceneDelegate.cs
@@ -12,9 +12,16 @@ namespace NewSingleViewTemplate {
 		[Export ("scene:willConnectToSession:options:")]
 		public void WillConnect (UIScene scene, UISceneSession session, UISceneConnectionOptions connectionOptions)
 		{
-			// Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-			// If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-			// This delegate does not imply the connecting scene or session are new (see UIApplicationDelegate `GetConfiguration` instead).
+			var IsFirsttime = NSUserDefaults.StandardUserDefaults.BoolForKey("IsFirsttime");
+			if (!IsFirsttime)
+			{
+				Window.RootViewController = UIStoryboard.FromName("Main", null).InstantiateViewController("WeightVC");
+				NSUserDefaults.StandardUserDefaults.SetBool(true, "IsFirsttime");
+			}
+			else
+			{
+				// Start from the default initialized view controller
+			}
 		}
 
 		[Export ("sceneDidDisconnect:")]
